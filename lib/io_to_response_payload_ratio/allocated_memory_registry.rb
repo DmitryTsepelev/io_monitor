41ae3289cac1c5d6ecc5f5c1cfe687c1d36cf79a
SupDex <symeton@gmail.com>
2022-03-21 21:00:01 +0300

    add AllocatedMemoryRegistry, party copy of ActiveRecord::RuntimeRegistry

---
+++ lib/io_to_response_payload_ratio/allocated_memory_registry.rb
@@ -0,0 +1,16 @@
+require "active_support/per_thread_registry"
+
+module IoToResponsePayloadRatio
+  class AllocatedMemoryRegistry
+    extend ActiveSupport::PerThreadRegistry
+
+    METHODS = %i[db_allocated_memory]
+
+    attr_accessor *METHODS
+
+    METHODS.each do |val|
+      class_eval %{ def self.#{val}; instance.#{val}; end }, __FILE__, __LINE__
+      class_eval %{ def self.#{val}=(x); instance.#{val}=x; end }, __FILE__, __LINE__
+    end
+  end
+end
