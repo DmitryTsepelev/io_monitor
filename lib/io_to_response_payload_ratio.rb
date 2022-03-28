3f5fdb004efc9c973039b1acc57b40c67b1c835a
SupDex <symeton@gmail.com>
2022-03-28 23:24:27 +0300

    remove debug and call notification

--- lib/io_to_response_payload_ratio.rb
+++ lib/io_to_response_payload_ratio.rb
@@ -4,6 +4,7 @@ require_relative "io_to_response_payload_ratio/configuration"
 require_relative "io_to_response_payload_ratio/measure_payload_size"
 require_relative "io_to_response_payload_ratio/railtie"
 require_relative "io_to_response_payload_ratio/extensions/active_record/extension"
+require_relative "io_to_response_payload_ratio/notifications/resolver"
 
 module IoToResponsePayloadRatio
   class Error < StandardError; end
@@ -25,6 +26,16 @@ module IoToResponsePayloadRatio
   end
 
   module Controller
+    def self.included(base)
+      return unless base < ActionController::Base
 
+      ActiveSupport::Notifications.subscribe('process_action.action_controller') do |*args|
+        event = ActiveSupport::Notifications::Event.new(*args)
+
+        return if event.payload[:controller] != base.to_s
+
+        Notifications::ActiveRecord::Notification.new(payload: event.payload).call
+      end
+    end
   end
 end
