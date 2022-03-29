729f8097d1a4ea2be2e1d66f56fc2973d16fcbfe
SupDex <symeton@gmail.com>
2022-03-29 21:18:41 +0300

    add spec for ratio

--- spec/io_to_response_payload_ratio_spec.rb
+++ spec/io_to_response_payload_ratio_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe IoToResponsePayloadRatio do
   it "has a version number" do
     expect(IoToResponsePayloadRatio::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
