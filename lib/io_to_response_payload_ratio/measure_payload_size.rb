3241e004313c0c4b8387882faaa3bb5cb2e426d4
SupDex <symeton@gmail.com>
2022-03-30 21:41:11 +0300

    remove ObjectSpace measuring

--- lib/io_to_response_payload_ratio/measure_payload_size.rb
+++ lib/io_to_response_payload_ratio/measure_payload_size.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "objspace"
-
 module IoToResponsePayloadRatio
   class MeasurePayloadSize
     attr_accessor :result, :payload_bytes
@@ -15,7 +13,6 @@ module IoToResponsePayloadRatio
         end
 
       self.payload_bytes = Marshal.dump(result).size
-      #self.payload_bytes = MeasureObjectSpace.new(result).size
     end
 
     def payload_size_in_bytes
@@ -26,50 +23,4 @@ module IoToResponsePayloadRatio
       (payload_bytes.to_f / 1024).round(2)
     end
   end
-
-  # class MeasureObjectSpace
-  #   attr_reader :size
-  #
-  #   SIMPLE_CLASSES = [String, Symbol, Integer, Float, Numeric, Date, Time, TrueClass, FalseClass].freeze
-  #   BASE_CLASSES = [Class, Object].freeze
-  #
-  #   def initialize(obj)
-  #     @size = 0
-  #
-  #     measuring_mem_size!(obj)
-  #   end
-  #
-  #   private
-  #
-  #   attr_writer :size
-  #
-  #   def measuring_mem_size!(object)
-  #     return if object.nil?
-  #
-  #     if SIMPLE_CLASSES.include?(object.class)
-  #       self.size += ObjectSpace.memsize_of(object)
-  #
-  #       return
-  #     end
-  #
-  #     if object.instance_of?(Array)
-  #       object.each do |obj|
-  #
-  #         measuring_mem_size!(obj)
-  #       end
-  #
-  #       return
-  #     end
-  #
-  #     ObjectSpace.reachable_objects_from(object).each do |related_object|
-  #       if related_object.is_a?(ObjectSpace::InternalObjectWrapper) || BASE_CLASSES.include?(related_object.class)
-  #         next
-  #       end
-  #
-  #       self.size += ObjectSpace.memsize_of(related_object)
-  #
-  #       measuring_mem_size!(related_object)
-  #     end
-  #   end
-  # end
 end
