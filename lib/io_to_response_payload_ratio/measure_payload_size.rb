6cd1751e8bd807c6911ffd6de2c46ac33ae9ff08
SupDex <symeton@gmail.com>
2022-03-30 21:06:42 +0300

    little bit refactoring

--- lib/io_to_response_payload_ratio/measure_payload_size.rb
+++ lib/io_to_response_payload_ratio/measure_payload_size.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'objspace'
+require "objspace"
 
 module IoToResponsePayloadRatio
   class MeasurePayloadSize
--- lib/io_to_response_payload_ratio/notifications/logs.rb
+++ lib/io_to_response_payload_ratio/notifications/logs.rb
@@ -6,7 +6,13 @@ module IoToResponsePayloadRatio
   module Notifications
     class Logs < Base
       def call
-        Rails.logger.warn("#{payload[:ratio_message]}. #{payload[:controller]}##{payload[:action]}")
+        Rails.logger.warn(build_message)
+      end
+
+      private
+
+      def build_message
+        "#{payload[:ratio_message]}. #{payload[:controller]}##{payload[:action]}"
       end
     end
   end
