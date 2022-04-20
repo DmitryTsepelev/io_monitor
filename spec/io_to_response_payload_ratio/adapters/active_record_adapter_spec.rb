76f1336d2af0bc20daebcd2bd36fe4664498db77
maxshend <shendrik.mv@gmail.com>
2022-04-21 01:25:48 +0300

    Add support for ActiveRecord::Relation#load_async method.

--- spec/io_to_response_payload_ratio/adapters/active_record_adapter_spec.rb
+++ spec/io_to_response_payload_ratio/adapters/active_record_adapter_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+require_relative "../../support/helpers/async_helper"
+
 RSpec.describe IoToResponsePayloadRatio::ActiveRecordAdapter do
+  include Helpers::AsyncHelper
+
   let(:aggregator) { IoToResponsePayloadRatio.aggregator }
 
   with_model :Fake, scope: :all do
@@ -27,23 +31,41 @@ RSpec.describe IoToResponsePayloadRatio::ActiveRecordAdapter do
     aggregator.stop!
   end
 
-  context "when aggregator is inactive" do
-    before do
-      aggregator.stop!
+  describe "without async queries" do
+    context "when aggregator is inactive" do
+      before do
+        aggregator.stop!
+      end
+
+      it "does nothing" do
+        expect(aggregator).not_to receive(:increment)
+
+        Fake.all.to_a
+      end
     end
 
-    it "does nothing" do
-      expect(aggregator).not_to receive(:increment)
+    it "increments aggregator by query result's bytesize" do
+      allow(aggregator).to receive(:increment)
 
-      Fake.all.to_a
+      bytesize = Fake.pluck(:id, :name).flatten.join.bytesize
+
+      expect(aggregator).to have_received(:increment).with(described_class.kind, bytesize)
     end
   end
 
-  it "increments aggregator by query result's bytesize" do
-    allow(aggregator).to receive(:increment)
+  if Rails::VERSION::MAJOR >= 7
+    context "when load_async is used" do
+      let!(:bytesize) { Fake.pluck(:id, :name).flatten.join.bytesize }
 
-    bytesize = Fake.pluck(:id, :name).flatten.join.bytesize
+      it "increments aggregator by query result's bytesize", skip_transaction: true do
+        allow(aggregator).to receive(:increment)
 
-    expect(aggregator).to have_received(:increment).with(described_class.kind, bytesize)
+        relation = Fake.all.load_async
+        wait_for_async_query(relation)
+        relation.to_a
+
+        expect(aggregator).to have_received(:increment).with(described_class.kind, bytesize)
+      end
+    end
   end
 end
