465401e7e5c89bdb7e02969891df60129a48aaa4
Andrey Novikov <envek@envek.name>
2022-05-31 14:13:46 +0300

    Test against various versions of Ruby on Rails: 6.1, 7.0, edge

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -15,7 +15,7 @@ module Dummy
     config.root = File.join(__dir__, "..")
     config.logger = Logger.new("/dev/null")
     config.api_only = true
-    config.active_record.legacy_connection_handling = false
+    config.active_record.legacy_connection_handling = false if ActiveRecord::Base.respond_to?(:legacy_connection_handling=)
 
     if Rails::VERSION::MAJOR >= 7
       config.active_record.async_query_executor = :global_thread_pool
